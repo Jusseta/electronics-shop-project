5c74b0075c0d3c3dc004e795abd94233da7b0707
Victoria Kashpur <vikki0011@gmail.com>
2023-06-11 19:53:54 +1000

    start hw2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os.path
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,15 +17,14 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.__class__.all.append(self)
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         total_price = self.price * self.quantity
@@ -32,3 +35,34 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            print('Длина наименования товара превышает 10 символов.')
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+        """
+        with open(os.path.join(os.path.dirname(__file__), 'items.csv'), newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for i in reader:
+                name, price, quantity = i['name'], float(i['price']), int(i['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(num):
+        """
+        статический метод, возвращающий число из числа-строки
+        """
+        number = int(float(num))
+        return number
